5fd037be55d49cb42ad04cfc6bd4ade79876110f
Ekene Ezema <ekenestanekeson@gmail.com>
2023-01-11 14:05:11 +0100

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,8 +6,16 @@ needed to reach a particular
 number (n) of 'H' xters from a 
 single xter"""
 
+
 def minOperations(n):
- 
+    """
+    A function that calculates the fewest number of operations
+    needed to give a result of exactly n H characters in a file
+    args: n: Number of characters to be displayed
+    return:
+          number of min operations
+    """
+
     now = 1
     start = 0
     counter = 0
